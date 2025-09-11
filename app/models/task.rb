f70462ae3fa787c82cfb061995eeca2627f95164
Tanya-boop41 <efita41@gmail.com>
2025-09-11 20:51:37 +0300

    Rename state machine events to action verbs

--- app/models/task.rb
+++ app/models/task.rb
@@ -14,15 +14,15 @@ class Task < ApplicationRecord
       transition [:new_task, :released] => :archived
     end
 
-    event :develop do
+    event :start_development do
       transition [:new_task, :in_qa, :in_code_review] => :in_development
     end
 
-    event :testing do
+    event :start_testing do
       transition in_development: :in_qa
     end
 
-    event :review do
+    event :start_review do
       transition in_qa: :in_code_review
     end
 
