b32bd45419c5869bef454857bae9e745afffda54
Tanya <efita41@gmail.com>
2025-09-18 11:42:16 +0300

    Add state machine to Task model (#7)
    * Add state machine to Task model
    
    * Rename state machine events to action verbs
    
    * Rename state machine events to action verbs version2

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,6 @@
 class Task < ApplicationRecord
+  include StateMachines::Activerecord
+
   belongs_to :author, class_name: 'User'
   belongs_to :assignee, class_name: 'User', optional: true
 
@@ -6,4 +8,31 @@ class Task < ApplicationRecord
   validates :description, presence: true
   validates :author, presence: true
   validates :description, length: { maximum: 500 }
+
+  state_machine initial: :new_task do
+    event :archive do
+      transition [:new_task, :released] => :archived
+    end
+
+    event :to_development do
+      transition [:new_task, :in_qa, :in_code_review] => :in_development
+    end
+
+    event :to_testing do
+      transition in_development: :in_qa
+    end
+
+    event :to_review do
+      transition in_qa: :in_code_review
+    end
+
+    event :prepare_for_release do
+      transition in_code_review: :ready_for_release
+    end
+
+    event :release do 
+      transition ready_for_release: :released
+    end
+    
+  end
 end
